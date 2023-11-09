f0d9d1dce15495421f74fb1a282730d9129891e9
kinki-kids <t.matsuzaki0727@gmail.com>
2023-11-09 15:17:04 +0900

    measure roll number 1

--- sample/sample.ino
+++ sample/sample.ino
@@ -17,11 +17,12 @@ void setup() {
   pinMode(motor_l2, OUTPUT);
   pinMode(pwm_motor_r, OUTPUT);
   pinMode(pwm_motor_l, OUTPUT);
-  bool pre_val0 = 0;
-  bool pre_val1 = 0;
+  bool pre_val0 = FALSE;
+  bool pre_val1 = FALSE;
   int count6 = 0;
-  int start_time;
-  int end_time;
+  double start_time;
+  double end_time;
+  double round_time;
 }
 
 void loop() {
@@ -30,30 +31,30 @@ void loop() {
   int val0 = analogRead(A0);//A0の値(センサ1)
   int val1 = analogRead(A1);//A1の値(センサ2)
   int val2 = analogRead(A2);
+  String str;
   pre_val0 = val2 > threshold;
   if(pre_val0 * pre_val1 < 0){
     count6++;
 
     if(count6 == 1){
-      start_time
+      start_time = DateTime.now();
     }
 
-    if(count6 > 6){
+    if(count6 == 7){
       count6 = 0;
+      end_time = DateTime.now();
+      round_time = (end_time - start_time) / 1000;
+      Serial.println("Rolling time:" + String(round_time)+ " s.");
     }
   }
 
-  String str = String(val0) + "," + String(val1);
+  // Serial.println(String(val0) + "," + String(val1));
 
-  Serial.println(str);
-
-  Serial.print(val0);
-  Serial.print(",");
-  Serial.print(val1);
-  Serial.print(",");
-  Serial.println(val2); 
-
-  
+  // Serial.print(val0);
+  // Serial.print(",");
+  // Serial.print(val1);
+  // Serial.print(",");
+  // Serial.println(val2);
 
   digitalWrite(motor_l1, HIGH);
   digitalWrite(motor_l2, LOW);
